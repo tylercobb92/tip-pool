7a3f6b1f22b80a42919fbe4e62f89ea5962d63d3
tyler <tylercobb92@gmail.com>
2021-08-24 16:21:12 -0400

    added test for updateServerTable; edited teardown logic for new error on submitting form while fields are blank

--- servers.test.js
+++ servers.test.js
@@ -11,11 +11,26 @@ describe("Servers test (with setup and tear-down)", function () {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it('should not add a new server to allServers on submitServerInfo() when input is blank', function () {
+    serverNameInput.value = '';
+    submitServerInfo();
+
+    expect(Object.keys(allServers).length).toEqual(0);
+  });
+
+  it('should update #servertable on updateServerTable()', function () {
+    submitServerInfo();
+    updateServerTable();
+    let chart = document.querySelectorAll('#serverTable tbody tr td');
+
+    expect(chart[0].textContent).toEqual('Alice');
+    expect(chart[1].textContent).toEqual('$0.00');
+  })
+
   afterEach(function () {
     // teardown logic
-    const table = document.getElementById("serverTable");
-    if (table.lastElementChild) {
-      table.lastElementChild.remove();
-    }
+    serverId = 0;
+    serverTbody.innerHTML = '';
+    allServers = {};
   });
 });
