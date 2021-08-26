3532f69ba76e2903380019ba427ed0fb1d2c36be
tyler <tylercobb92@gmail.com>
2021-08-26 14:24:09 -0400

    added tests for appendTd(), calculateTipPercent(), and sumPaymentTotal()

--- helpers.test.js
+++ helpers.test.js
@@ -5,9 +5,39 @@ describe("payment info (with setup and teardown)", function () {
         submitPaymentInfo();
     });
 
-    it('should calculate tip percent on sumPaymentTotal()', function () {
+    it('should calculate tip percent of single transaction on calculateTipPercent(billAmt, tipAmt)', function () {
+        expect(calculateTipPercent(100, 18)).toEqual(18);
+        expect(calculateTipPercent(1, 0.08)).toEqual(8);
+    })
+
+    it('should calculate total tip percent on sumPaymentTotal()', function () {
         expect(sumPaymentTotal('tipPercent')).toEqual(20);
     });
+
+    it('should calculate total bill amount on sumPaymentTotal()', function () {
+        expect(sumPaymentTotal('billAmt')).toEqual(50);
+
+        billAmtInput.value = 25;
+        tipAmtInput.value = 5;
+        submitPaymentInfo();
+        expect(sumPaymentTotal('billAmt')).toEqual(75);
+    })
+
+    it('should calculate total tip amount on sumPaymentTotal()', function () {
+        expect(sumPaymentTotal('tipAmt')).toEqual(10);
+
+        billAmtInput.value = 25;
+        tipAmtInput.value = 5;
+        submitPaymentInfo();
+        expect(sumPaymentTotal('tipAmt')).toEqual(15);
+    })
+
+    it('should create and append a new td to tr on appendTd(tr, value)', function () {
+        let newTr = document.createElement('tr');
+        appendTd(newTr, 'test');
+
+        expect(newTr.children.length).toEqual(1);
+    })
 
     afterEach(function () {
         billAmtInput.value = '';
@@ -16,5 +46,7 @@ describe("payment info (with setup and teardown)", function () {
         summaryTds[0].innerHTML = "";
         summaryTds[1].innerHTML = "";
         summaryTds[2].innerHTML = "";
+        allPayments = {};
+        paymentId = 0;
     })
 })
